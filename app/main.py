84ad696e05be628da6afaf1eab07c51a59d4fa6d
aniketh1409 <anikeths@ualberta.ca>
2026-01-30 00:08:46 -0700

    front end visual entrypoint

--- app/main.py
+++ app/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from .db.models import Base
 from .db.session import engine
 from .routers.applications import router as applications_router
+from fastapi.staticfiles import StaticFiles
+from .routers.ui import router as ui_router
+
 
 app = FastAPI(title="Job Tracker API")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.include_router(ui_router)
 
 # For MVP: create tables on startup (later you can swap to Alembic migrations)
 Base.metadata.create_all(bind=engine)
